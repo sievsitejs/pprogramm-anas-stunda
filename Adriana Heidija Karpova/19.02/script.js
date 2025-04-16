b039332c16f738a64cb3c52d599117ea1415cdac
kristiine302 <kevinsdzeims@gmail.com>
2025-04-16 11:09:26 +0300

    KD02
    16.04

---
+++ Adriana Heidija Karpova/19.02/script.js
@@ -0,0 +1,12 @@
+let x = 1;
+
+switch(x)
+{
+    case 0:
+        console.log("x = 0");
+        break;
+
+        case 1:
+            comsole.log("x = 1");
+            break;
+}
---
+++ Adriana Heidija Karpova/KD02/script.js
@@ -0,0 +1,40 @@
+/* let age = parseInt (prompt ("age: "));
+let pilsoniba = false;
+if ((age >=18 ) && (pilsoniba == true));
+{
+    console.log("Drīkst balsot.")
+}
+if ((age >=18 ) && (pilsoniba == false))
+{
+    console.log("Ir pilngadīgs, bet nav pilsonis.")
+}
+else ((age < 18 ) && (pilsoniba == false));
+{
+    console.log("Nav pilngadīgs un nav pilsonis.")
+} 
+    if ((age < 18 ) && (pilsoniba == true))
+{
+    console.log("Nav pilngadīgs, bet ir pilsonis.")
+}*/
+
+
+/* let atzime = parseInt (prompt ("ievadi atzimi (0-100): "));
+if ((atzime >= 90 ));
+{
+    console.log("Izcili!");
+}
+if ((atzime >=70));
+{
+    console.log("labi!");
+}
+if ((atzime >=50));
+{
+    console.log("vidēji!");
+}
+if ((atzime >=0));
+{
+    console.log("slikti!");
+} */
+
+
+    
